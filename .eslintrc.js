3e54a42fb240ab94a2a6b24efe9272d242cc6c63
Gabriel Alves <gabe.alvesch@gmail.com>
2021-10-20 04:20:37 -0300

    fix: dot files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,16 @@
 module.exports = {
-    "parser": "@typescript-eslint/parser",
-    "env": {
-        "es2021": true,
-        "node": true
-    },
-    "plugins": ["@typescript-eslint"],
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  parser: "@typescript-eslint/parser",
+  env: {
+    es2021: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
 };
