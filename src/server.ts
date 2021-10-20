0d730c90326c4d190d7e5a9efa75ae7474e01013
Gabriel Alves <gabriel@ilandstudios.com>
2021-10-20 14:26:08 -0300

    fix: changed PORT env var

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const http = new Http(parseInt(process.env.HTTP_PORT) || 4000);
+const http = new Http(parseInt(process.env.PORT) || 4000);
 
 http.start();
